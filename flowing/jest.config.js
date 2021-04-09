3b8754e2bf9ac232c55ff3ee5ae0373c609937a5
Antonio Dell'Ava <antonio.dellava@gmail.com>
2021-04-09 15:17:59 +0200

    step 2

---
+++ flowing/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/hero-app',
+    '<rootDir>/libs/hero/data-access',
+    '<rootDir>/libs/hero/ui',
+    '<rootDir>/libs/hero/controllers',
+    '<rootDir>/apps/hero-api',
+  ],
+};
